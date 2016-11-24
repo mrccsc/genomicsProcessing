eee07782312fdd1859e73ec90bfdd42e1d6947aa
yfwang27 <yfwang27@gmail.com>
2016-11-24 16:21:08 +0000

    revise

--- pipeline.r
+++ pipeline.r
@@ -22,7 +22,6 @@ readyBasecalling <- function(Run_folders_WithRTA,subFoldersFull,config,bclVersio
         # (2) check Project integrity
             # "UP": Unknown project
             ss[ss$Project==""|ss$Project==" "|is.na(ss$Project),]$Project<-"UP"
-            
         # (3) a. check the index sequence in $Index column, i.e. remove the space character
             ss$Index <- gsub(" ","",ss$Index)
             # b. check the index sequence length in $Index column
@@ -54,9 +53,7 @@ readyBasecalling <- function(Run_folders_WithRTA,subFoldersFull,config,bclVersio
                 #ss$SampleID <- gsub("[[:punct:]]", "_", ss$SampleID).)
                 #ss$SampleID <- gsub("[^[:alnum:]]", "_", ss$SampleID).)
             }
-        
         indexLoop <- 1  # unused parameter; for future development
-        
         # (6) generate sample sheets and shell scripts for different index length combination
         for(l in uniqueIndexTypes){
             tempss <- ss[allIndexTypes %in% l,]
@@ -96,81 +93,78 @@ readyBasecalling <- function(Run_folders_WithRTA,subFoldersFull,config,bclVersio
           #  dir.create(gsub("\\.csv",paste0("_",l),sampleSheetName),showWarnings = F)
           #}
         # generate shell script for qsub ####################################################################
-        if(!file.exists(gsub("\\.csv",paste0("_",l,"\\.sh"),sampleSheetName))){
-            indexLengths <- unlist(strsplit(l,"-"))  # this is for the dual-index situation, i.e. l="6-0"
-            # define the parameters for "--use-base-mask" ################################################
-              # a. define the parameter for IndexRead1: indexMask1
-              if(as.numeric(as.vector(currentRunParameters$IndexRead1)) != 0){
-                  numberOfNs <- as.numeric(as.vector(currentRunParameters$IndexRead1)) - as.numeric(indexLengths[1])
-                  if(indexLengths[1] == 0){
-                    indexMask1 <- ",n*"
-                  }else{
-                    indexMask1 <-  paste0(",I",indexLengths[1],paste0(rep("n",numberOfNs),collapse=""))
-                  }
+          if(!file.exists(gsub("\\.csv",paste0("_",l,"\\.sh"),sampleSheetName))){
+              indexLengths <- unlist(strsplit(l,"-"))  # this is for the dual-index situation, i.e. l="6-0"
+              # define the parameters for "--use-base-mask" ################################################
+                # a. define the parameter for IndexRead1: indexMask1
+                if(as.numeric(as.vector(currentRunParameters$IndexRead1)) != 0){
+                    numberOfNs <- as.numeric(as.vector(currentRunParameters$IndexRead1)) - as.numeric(indexLengths[1])
+                    if(indexLengths[1] == 0){
+                      indexMask1 <- ",n*"
+                    }else{
+                      indexMask1 <-  paste0(",I",indexLengths[1],paste0(rep("n",numberOfNs),collapse=""))
+                    }
+                }else{
+                    indexMask1 <- ""
+                }
+                # b. define the parameter for IndexRead2: indexMask2
+                if(as.numeric(as.vector(currentRunParameters$IndexRead2)) != 0){
+                    if (length(indexLengths)==1){
+                      indexLengths[2]=0
+                    }
+                    numberOfNs <- as.numeric(as.vector(currentRunParameters$IndexRead2)) - as.numeric(indexLengths[2])
+                    if(indexLengths[2] == 0){
+                      indexMask2 <- ",n*"
+                    }else{
+                      indexMask2 <-  paste0(",I",indexLengths[2],paste0(rep("n",numberOfNs),collapse=""))
+                    }
+                }else{
+                    indexMask2 <- ""
+                }
+                # c. define the parameter for single end or paired end
+                if(currentRunParameters$Read2 > 0){  # paired end
+                    usebasemask <- paste0("y*n",indexMask1,indexMask2,",y*n")
+                }else{     # single end
+                    usebasemask <- paste0("y*n",indexMask1,indexMask2)
+                }
+             # define the script for the analysis
+              if(bclVersion == "Old"){
+                  programToRun <- as.vector(config[config$section == "programs" & config$name == "configureBclToFastq","value"])
+                  runBCLcommand <- paste0(
+                            programToRun,
+                            " --input-dir ",
+                            file.path(currentRun,as.vector(config[config$section == "paths" & config$name == "inputdir","value"])),
+                            " --sample-sheet ",
+                            gsub("\\.csv",paste0("_",l,"\\.csv"),sampleSheetName),
+                            " --fastq-cluster-count=0 --use-bases-mask ",usebasemask,
+                            " --output-dir ",
+                            gsub("\\.csv",paste0("_",l),sampleSheetName),
+                            "\n")
               }else{
-                  indexMask1 <- ""
+                  programToRun <- as.vector(config[config$section == "programs" & config$name == "BclToFastq2","value"])
+                  runBCLcommand <- paste0(
+                            programToRun,
+                            " --input-dir ",
+                            file.path(currentRun,as.vector(config[config$section == "paths" & config$name == "inputdir","value"])),
+                            " --sample-sheet ",
+                            gsub("\\.csv",paste0("_",l,"\\.csv"),sampleSheetName),
+                            " --use-bases-mask ",usebasemask,
+                            " -r 4 -p 4 -w 4 -d 4 --output-dir ",
+                            gsub("\\.csv",paste0("_",l),sampleSheetName),
+                            "\n")
               }
-              # b. define the parameter for IndexRead2: indexMask2
-              if(as.numeric(as.vector(currentRunParameters$IndexRead2)) != 0){
-                  if (length(indexLengths)==1){
-                    indexLengths[2]=0
-                  }
-                  numberOfNs <- as.numeric(as.vector(currentRunParameters$IndexRead2)) - as.numeric(indexLengths[2])
-                  if(indexLengths[2] == 0){
-                    indexMask2 <- ",n*"
-                  }else{
-                    indexMask2 <-  paste0(",I",indexLengths[2],paste0(rep("n",numberOfNs),collapse=""))
-                  }
-              }else{
-                  indexMask2 <- ""
-              }
-              # c. define the parameter for single end or paired end
-              if(currentRunParameters$Read2 > 0){  # paired end
-                  usebasemask <- paste0("y*n",indexMask1,indexMask2,",y*n")
-              }else{     # single end
-                  usebasemask <- paste0("y*n",indexMask1,indexMask2)
-              }
-            #######################################################################################################
-            if(bclVersion == "Old"){
-                programToRun <- as.vector(config[config$section == "programs" & config$name == "configureBclToFastq","value"])
-                runBCLcommand <- paste0(
-                          programToRun,
-                          " --input-dir ",
-                          file.path(currentRun,as.vector(config[config$section == "paths" & config$name == "inputdir","value"])),
-                          " --sample-sheet ",
-                          gsub("\\.csv",paste0("_",l,"\\.csv"),sampleSheetName),
-                          " --fastq-cluster-count=0 --use-bases-mask ",usebasemask,
-                          " --output-dir ",
-                          gsub("\\.csv",paste0("_",l),sampleSheetName),
-                          "\n")
-            }else{
-                programToRun <- as.vector(config[config$section == "programs" & config$name == "BclToFastq2","value"])
-                runBCLcommand <- paste0(
-                          programToRun,
-                          " --input-dir ",
-                          file.path(currentRun,as.vector(config[config$section == "paths" & config$name == "inputdir","value"])),
-                          " --sample-sheet ",
-                          gsub("\\.csv",paste0("_",l,"\\.csv"),sampleSheetName),
-                          " --use-bases-mask ",usebasemask,
-                          " -r 4 -p 4 -w 4 -d 4 --output-dir ",
-                          gsub("\\.csv",paste0("_",l),sampleSheetName),
-                          "\n")
-            }
-          
-          
-            write.table("#!/bin/bash",file=gsub("\\.csv",paste0("_",l,"\\.sh"),sampleSheetName),col.names=F,row.names=F,append=F,quote=F)
-            write.table(runBCLcommand,file=gsub("\\.csv",paste0("_",l,"\\.sh"),sampleSheetName),col.names=F,row.names=F,append=T,quote=F)
-            write.table("#!/bin/bash",file=gsub("\\.csv",paste0("_",l,"ForQSUB\\.sh"),sampleSheetName),col.names=F,row.names=F,append=F,quote=F)
-            write.table(paste0("",gsub("\\.csv",paste0("_",l,"\\.sh"),sampleSheetName)),
-                        file=gsub("\\.csv",paste0("_",l,"ForQSUB\\.sh"),sampleSheetName),
-                        col.names=F,row.names=F,append=T,quote=F)
-            
-            shellBCLs[p] <- gsub("\\.csv",paste0("_",l,"ForQSUB\\.sh"),sampleSheetName)
-            p <- p+1
+              write.table("#!/bin/bash",file=gsub("\\.csv",paste0("_",l,"\\.sh"),sampleSheetName),col.names=F,row.names=F,append=F,quote=F)
+              write.table(runBCLcommand,file=gsub("\\.csv",paste0("_",l,"\\.sh"),sampleSheetName),col.names=F,row.names=F,append=T,quote=F)
+              write.table("#!/bin/bash",file=gsub("\\.csv",paste0("_",l,"ForQSUB\\.sh"),sampleSheetName),col.names=F,row.names=F,append=F,quote=F)
+              write.table(paste0("",gsub("\\.csv",paste0("_",l,"\\.sh"),sampleSheetName)),
+                          file=gsub("\\.csv",paste0("_",l,"ForQSUB\\.sh"),sampleSheetName),
+                          col.names=F,row.names=F,append=T,quote=F)
+              shellBCLs[p] <- gsub("\\.csv",paste0("_",l,"ForQSUB\\.sh"),sampleSheetName)
+              p <- p+1
+          }
+        #####################################################################
         }
-      }
     }else{
-      
       stop("No samplesheet ",basename(sampleSheetName)," discovered for run ",basename(currentRun))
     }
   }
@@ -203,7 +197,7 @@ p <- 1
 
 
 bclcommands <- readyBasecalling(Run_folders_WithRTA,subFoldersFull,config,bclVersion="New")
-qsubscommands <- makeQsubs(bclcommands,launch=TRUE)
+qsubscommands <- makeQsubs(bclcommands,launch=F)
 
 qsubscommands
 shellBCLs
