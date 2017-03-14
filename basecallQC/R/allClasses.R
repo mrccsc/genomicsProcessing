6c22a1b1ef55b74020aa13cf914d52454facae06
Travis CI <nobody@nobody.org>
2017-03-14 14:11:12 +0000

    .

--- basecallQC/R/allClasses.R
+++ basecallQC/R/allClasses.R
@@ -53,3 +53,35 @@ demultiplexMetrics <- function(params){
   return(c(params,list(demuxStatsProcessed=demuxStatsProcessed,
                        summarisedDemuxStats=summarisedDemuxStats)))
 }
+
+validateBCLSheet <- function(sampleSheet){
+  ss <- fread(sampleSheet,sep=",",header=T,stringsAsFactors=F)
+  ss$SampleID <- gsub("^X","Sample_",validNames(ss$SampleID))
+  ss$SampleID <-gsub("\\?|\\(|\\)|\\[|\\]|\\\\|/|\\=|\\+|<|>|\\:|\\;|\"|\'|\\*|\\^|\\||\\&|\\.","_",ss$SampleID)
+  ss$Index <- gsub(" ","",ss$Index)
+  toBeTrimmed <- which(lapply(ss$Index,nchar) > as.numeric(currentRunParameters$IndexRead1))
+  for(i in toBeTrimmed){
+    ss$Index[toBeTrimmed] <-  substr(ss$Index[toBeTrimmed],0,as.numeric(currentRunParameters$IndexRead1))
+  }
+
+  message("Read samplesheet ",basename(sampleSheetName)," discovered for run ",currentRun)
+  index1Lengths <- unlist(lapply(ss$Index,function(x)nchar(x)))
+
+  if(any(colnames(ss) %in% "Index2")){
+    ss$Index2 <- gsub(" ","",ss$Index2)
+    index2Lengths <- unlist(lapply(ss$Index2,function(x)nchar(x)))
+    index2NAs <- unlist(lapply(ss$Index2,function(x)is.na(x)))
+    index2Lengths[index2NAs] <- 0
+    allIndexTypes <- paste0(index1Lengths,"-",index2Lengths)
+    uniqueIndexTypes <- unique(allIndexTypes)
+    #ss$SampleID <- gsub("[[:punct:]]", "_", ss$SampleID).)
+    #ss$SampleID <- gsub("[^[:alnum:]]", "_", ss$SampleID).)
+    ss$Index <- gsub("-NA|-$|^-$","",paste(ss$Index,ss$Index2,sep="-"))
+    ss <- ss[,-grep("Index2",colnames(ss))]
+  }else{
+    allIndexTypes <- paste0(index1Lengths)
+    uniqueIndexTypes <- unique(allIndexTypes)
+    #ss$SampleID <- gsub("[[:punct:]]", "_", ss$SampleID).)
+    #ss$SampleID <- gsub("[^[:alnum:]]", "_", ss$SampleID).)
+  }
+}
