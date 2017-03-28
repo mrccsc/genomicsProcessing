b6975b399de6d2013e9abeb53071a6cd47c2c32e
Thomas Carroll <tc.infomatics@gmail.com>
2017-03-28 11:59:23 +0100

    .

--- basecallQC/vignettes/Untitled.R
+++ basecallQC/vignettes/Untitled.R
@@ -22,3 +22,21 @@ baseMasks
 toSubmit <- createBCLcommand(bcl2fastqparams,cleanedSampleSheet,baseMasks)
 toSubmit
 
+## ---- basecallQC, fig.show='hold',warning=FALSE,message=FALSE------------
+fileLocations <- system.file("extdata",package="basecallQC")
+runXML <- dir(fileLocations,pattern="runParameters.xml",full.names=TRUE)
+config <- dir(fileLocations,pattern="config.ini",full.names=TRUE)
+sampleSheet <- dir(fileLocations,pattern="*\\.csv",full.names=TRUE)
+outDir <- file.path(fileLocations,"Runs/161105_D00467_0205_AC9L0AANXX/C9L0AANXX/")
+bcl2fastqparams <- setBCL2FastQparams(runXML,config,runDir=getwd(),outDir,verbose=FALSE)
+bclQC <- basecallQC(bcl2fastqparams,RunMetaData=NULL,sampleSheet)
+class(bclQC)
+
+## ---- tables, fig.show='hold',warning=FALSE,message=FALSE----------------
+summaryConvStatsTable(bclQC,output = "html")
+summaryDemuxTable(bclQC,output = "html")
+
+## ---- plots1, fig.show='hold',warning=FALSE,message=FALSE, fig.width=5, fig.height=5----
+passFilterBar(bclQC,groupBy="Sample")
+
+
